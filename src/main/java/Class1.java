dfbac91c2f9c3741796dfe43a0cd1d2704662a91
hallerturk <halilerturk91@gmail.com>
2023-08-15 01:16:04 -0400

    versionn2

---
+++ src/main/java/Class1.java
@@ -0,0 +1,11 @@
+public class Class1 {
+    public static void main(String[] args) {
+        System.out.println("version 1");
+        System.out.println("new version 1");
+        System.out.println("new version 2");
+        System.out.println("new version 3");
+        System.out.println("ttt");
+        System.out.println("sda");
+        System.out.println("ddd");
+    }
+}
